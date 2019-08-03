168a2a775696e80a7966491986381c0a856d829d
zjthomas <zachthomas809@gmail.com>
2019-08-02 19:07:36 -0700

    add cacheing with redis

--- server/index.js
+++ server/index.js
@@ -1,18 +1,37 @@
 /* eslint-disable import/newline-after-import */
+require('newrelic');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const redis = require('redis');
 const path = require('path');
 const model = require('./db/model');
 
 const port = 3000;
+const REDIS_PORT = process.env.REDIS_PORT;
+
+const client = redis.createClient(REDIS_PORT);
+
+const cache = (req, res, next) => {
+  const listing = req.params.listingId;
+  client.get(listing, (err, data) => {
+    if (err) throw err;
+    if (data !== null) {
+      res.send(data);
+    } else {
+      next();
+    }
+  });
+};
 
 app.use('/:listingid', express.static(path.join(__dirname, '../client/dist')));
 
-app.get('/:listingId/reviews', (req, res) => {
-  console.log('GET:', req.params);
-  model.getByListing(req.params.listingId)
+app.get('/:listingId/reviews', cache, (req, res) => {
+  // console.log('GET:', req.params);
+  const listing = req.params.listingId;
+  model.getByListing(listing)
     .then((data) => {
+      client.setex(listing, 300, JSON.stringify(data.rows));
       res.send(JSON.stringify(data.rows));
     })
     .catch((err) => {
